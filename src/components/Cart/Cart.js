7b5a830182bb6ece7bc1c08da5827cd74599b43c
srishtirathi <srishti.cs17@bmsce.ac.in>
2021-03-07 16:43:38 +0530

    refactor(Cart): format code

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,68 +3,67 @@ import './Cart.css';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const Cart = ({ cartItems, cartCount }) => {
-  if (cartCount === 0) {
-    return 'Your cart is empty';
-  }
-  return (
+const Cart = ({ cartItems, cartCount }) => (
 
-    <div>
-      <h1>
-        Your basket has
-        {' '}
-        {/* {' '}
+  <div>
+    {cartCount === 0 ? <h1>Your cart is empty</h1> : (
+      <div>
+        <h1>
+          Your basket has
+          {' '}
+          {/* {' '}
         {Object.values(cartItems).reduce((prev, curr) => prev.quantity + curr.quantity)}
         {' '} */}
-        {cartCount }
-        {' '}
-        items.
-      </h1>
-      <hr />
-      <table className="cart-table">
-        <tr>
-          <th>Item Description</th>
-          <th>Unit Price</th>
-          <th>Quantity</th>
-          <th>Subtotal</th>
-
-        </tr>
-
-        {cartItems.map((item) => (
+          {cartCount }
+          {' '}
+          items.
+        </h1>
+        <hr />
+        <table className="cart-table">
           <tr>
-            <td>{item.product}</td>
-            <td>{item.price}</td>
-            <td>{item.quantity}</td>
-            <td>{item.price * item.quantity}</td>
+            <th>Item Description</th>
+            <th>Unit Price</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+
           </tr>
 
-        ))}
+          {cartItems.map((item) => (
+            <tr>
+              <td>{item.product}</td>
+              <td>{item.price}</td>
+              <td>{item.quantity}</td>
+              <td>{item.price * item.quantity}</td>
+            </tr>
 
-      </table>
-      <div className="body-container">
-        <div>
-          <Link to="/"><button type="button" className="continue-button">Continue Shopping!</button></Link>
-        </div>
-        <div className="cart-card">
-          <div className="cart-container">
-            <h4><b>Total</b></h4>
-            <p>
-              {Object.values(cartItems).reduce(
-                (prev, curr) => prev.price * prev.quantity + curr.price * curr.quantity,
-              )}
-            </p>
-          </div>
-          <hr />
+          ))}
+
+        </table>
+        <div className="body-container">
           <div>
-            <Link to="/container"><button type="button" className="checkout-button">Checkout</button></Link>
+            <Link to="/"><button type="button" className="continue-button">Continue Shopping!</button></Link>
+          </div>
+          <div className="cart-card">
+            <div className="cart-container">
+              <h4><b>Total</b></h4>
+              <p>
+                {Object.values(cartItems).reduce(
+                  (prev, curr) => prev.price * prev.quantity + curr.price * curr.quantity,
+                )}
+              </p>
+            </div>
+            <hr />
+            <div>
+              <Link to="/container"><button type="button" className="checkout-button">Checkout</button></Link>
+            </div>
           </div>
         </div>
       </div>
+    )}
 
-    </div>
+  </div>
 
-  );
-};
+);
 
 Cart.propTypes = {
   cartCount: PropTypes.number.isRequired,
