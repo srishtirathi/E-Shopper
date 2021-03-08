f31f37da859c8662c7d00bd83ce74aa64491e8fc
srishtirathi <srishti.cs17@bmsce.ac.in>
2021-03-08 13:59:48 +0530

    refactor(Checkout.js): format code,change class component to function and use hooks

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,35 +1,27 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './Checkout.css';
 import { withRouter } from 'react-router-dom';
 
-class Checkout extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: '',
-      address: '',
-      contact: '',
-      email: '',
-      message: '',
-    };
-  }
+const Checkout = () => {
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [contact, setContact] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  // const handleChange = (event) => {
+  // // if (event.target.name === 'contact' && this.target.value.length > 10) {
+  // //   this.setState({ [event.target.name]: event.target.value, message: 'Not valid' });
+  // // } else {
+  //   this.setState((prevState) =>
+  // ({ ...prevState, [event.target.name]: event.target.value }), () => {
+  //     const { name, phone } = this.state;
+  //     console.log(name, phone);
+  //   });
+  // };
+  // }
 
-handleChange=(event) => {
-  // if (event.target.name === 'contact' && this.target.value.length > 10) {
-  //   this.setState({ [event.target.name]: event.target.value, message: 'Not valid' });
-  // } else {
-  this.setState((prevState) => ({ ...prevState, [event.target.name]: event.target.value }), () => {
-    const { name, phone } = this.state;
-    console.log(name, phone);
-  });
-}
-// }
-
-render() {
-  const {
-    name, email, address, contact, message,
-  } = this.state;
   return (
 
     <div className="checkout-card">
@@ -39,23 +31,23 @@ render() {
           <label>
             Name:
           </label>
-          <input type="text" name="name" value={name} onChange={this.handleChange} />
+          <input type="text" name="name" value={name} onChange={(event) => { setName(event.target.value); }} />
           <br />
           <label>
             E-mail:
           </label>
 
-          <input type="email" name="email" value={email} onChange={this.handleChange} />
+          <input type="email" name="email" value={email} onChange={(event) => { setEmail(event.target.value); }} />
           <br />
           <label>
             Address:
           </label>
-          <input type="text" name="address" value={address} onChange={this.handleChange} />
+          <input type="text" name="address" value={address} onChange={(event) => { setAddress(event.target.value); }} />
           <br />
           <label>
             Contact Number:
           </label>
-          <input type="text" name="contact" value={contact} pattern="[0-9]{10}" onChange={this.handleChange} />
+          <input type="text" name="contact" value={contact} pattern="[0-9]{10}" onChange={(event) => { setContact(event.target.value); }} />
           <br />
           <br />
           <input type="submit" value="submit" />
@@ -64,6 +56,5 @@ render() {
       </div>
     </div>
   );
-}
-}
+};
 export default withRouter(Checkout);
